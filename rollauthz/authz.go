4e25f8da3e03afe30b301bbc2ec10ee302784514
d-smith <doug.smith@fmr.com>
2016-03-09 08:35:41 -0800

    Added config check test, set of config for testing

--- rollauthz/authz.go
+++ rollauthz/authz.go
@@ -2,8 +2,50 @@ package rollauthz
 import (
 	"strings"
 	log "github.com/Sirupsen/logrus"
-	"errors"
+	"os"
 )
+
+var vaultEndpoint string
+var vaultToken string
+
+func init() {
+	initFromEnv()
+}
+
+//We provide a function to call from init so we can manipulate the config settings in unit tests
+func initFromEnv() {
+	vaultEndpoint = os.Getenv("VAULT_ADDR")
+	if vaultEndpoint == "" {
+		log.Warn("Missing Configuration: VAULT_ADDR env variable not specified")
+	}
+
+	vaultToken = os.Getenv("VAULT_TOKEN")
+	if vaultToken == "" {
+		log.Warn("Missing configuration: VAULT_TOKEN env variable not specified")
+	}
+}
+
+type ErrConfig struct {}
+
+func (ec ErrConfig) Error() string {
+	return "rollauthz msiconfigured: VAULT_ADDR and VAULT_TOKEN environment variables must be specified"
+}
+
+type ErrNonBearerHeader struct {}
+
+func (e ErrNonBearerHeader) Error() string {
+	return "unexpected authorization header format - expecting bearer token"
+}
+
+func checkConfig() error {
+	if vaultEndpoint == "" || vaultToken == "" {
+		return ErrConfig{}
+	}
+
+	return nil
+}
+
+
 
 
 //ValidAccessToken takes an authorization header value, and, if the authorization header has
@@ -11,12 +53,15 @@ import (
 //if it was signed with the key associated with the aud claim, and passes other tests
 //of well-formed-ness.
 func ValidAccessToken(authzHeader string) (map[string]interface{},error) {
+	if err := checkConfig(); err != nil {
+		return nil,err
+	}
 
 	//Header format should be Bearer token
 	parts := strings.SplitAfter(authzHeader, "Bearer")
 	if len(parts) != 2 {
 		log.Info("Unexpected authorization header format - expecting bearer token")
-		return nil,errors.New("Unexpected authorization header format - expecting bearer token")
+		return nil,ErrNonBearerHeader{}
 	}
 
 	//Parse the token
--- rollauthz/authz_test.go
+++ rollauthz/authz_test.go
@@ -2,10 +2,30 @@ package rollauthz
 import (
 	"testing"
 	"github.com/stretchr/testify/assert"
+	"os"
 )
 
-func TestNonBearerTokenHeaderValusRejected(t *testing.T) {
+func TestMisconfiguration(t *testing.T) {
+	os.Unsetenv("VAULT_ADDR")
+	os.Unsetenv("VAULT_TOKEN")
+
+	_, err := ValidAccessToken("sure")
+	if assert.NotNil(t, err) {
+		_, ok := err.(ErrConfig)
+		assert.True(t, ok)
+	}
+}
+
+func TestNonBearerTokenHeaderValuesRejected(t *testing.T) {
+	os.Setenv("VAULT_ADDR", "config")
+	os.Setenv("VAULT_TOKEN", "config")
+	initFromEnv()
+
+
 	claims, err := ValidAccessToken("not a bearer token beader")
 	assert.Nil(t,claims)
-	assert.NotNil(t, err)
+	if assert.NotNil(t, err) {
+		_,ok := err.(ErrNonBearerHeader)
+		assert.True(t, ok, "expected a ErrNonBearerHeader error")
+	}
 }
