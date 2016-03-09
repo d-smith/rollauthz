9d3b0c3ce161db463b1d9c4893607a9570813036
d-smith <doug.smith@fmr.com>
2016-03-09 12:20:08 -0800

    Added token parse
    Signed-off-by: d-smith <doug.smith@fmr.com>

--- rollauthz/authz.go
+++ rollauthz/authz.go
@@ -1,70 +1,58 @@
 package rollauthz
+
 import (
-	"strings"
+	"fmt"
 	log "github.com/Sirupsen/logrus"
-	"os"
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/xtraclabs/rollsecrets/secrets"
+	rolltoken "github.com/xtraclabs/rollsecrets/token"
+	"strings"
 )
 
-var vaultEndpoint string
-var vaultToken string
-
-func init() {
-	initFromEnv()
+type RollAuthZ struct {
+	SecretsRepo secrets.SecretsRepo
 }
 
-//We provide a function to call from init so we can manipulate the config settings in unit tests
-func initFromEnv() {
-	vaultEndpoint = os.Getenv("VAULT_ADDR")
-	if vaultEndpoint == "" {
-		log.Warn("Missing Configuration: VAULT_ADDR env variable not specified")
-	}
-
-	vaultToken = os.Getenv("VAULT_TOKEN")
-	if vaultToken == "" {
-		log.Warn("Missing configuration: VAULT_TOKEN env variable not specified")
-	}
-}
-
-type ErrConfig struct {}
-
-func (ec ErrConfig) Error() string {
-	return "rollauthz msiconfigured: VAULT_ADDR and VAULT_TOKEN environment variables must be specified"
-}
-
-type ErrNonBearerHeader struct {}
+type ErrNonBearerHeader struct{}
 
 func (e ErrNonBearerHeader) Error() string {
 	return "unexpected authorization header format - expecting bearer token"
 }
 
-func checkConfig() error {
-	if vaultEndpoint == "" || vaultToken == "" {
-		return ErrConfig{}
-	}
+type ErrNoVaultClient struct{}
 
-	return nil
+func (e ErrNoVaultClient) Error() string {
+	return "no vault client configured for RollAuthZ"
 }
 
+type ErrParse struct {
+	source error
+}
 
+func (e ErrParse) Error() string {
+	return fmt.Sprintf("error parsing token: %s", e.source)
+}
 
-
-//ValidAccessToken takes an authorization header value, and, if the authorization header has
+//ValidateAccessToken takes an authorization header value, and, if the authorization header has
 //a JWT bearer token, returns the claims in the token is it is valid. A token is valid
 //if it was signed with the key associated with the aud claim, and passes other tests
 //of well-formed-ness.
-func ValidAccessToken(authzHeader string) (map[string]interface{},error) {
-	if err := checkConfig(); err != nil {
-		return nil,err
-	}
+func (raz RollAuthZ) ValidateAccessToken(authzHeader string) (map[string]interface{}, error) {
 
 	//Header format should be Bearer token
 	parts := strings.SplitAfter(authzHeader, "Bearer")
 	if len(parts) != 2 {
 		log.Info("Unexpected authorization header format - expecting bearer token")
-		return nil,ErrNonBearerHeader{}
+		return nil, ErrNonBearerHeader{}
 	}
 
 	//Parse the token
+	bearerToken := strings.TrimSpace(parts[1])
+	token, err := jwt.Parse(bearerToken, rolltoken.GenerateKeyExtractionFunction(raz.SecretsRepo))
+	if err != nil {
+		return nil, ErrParse{err}
+	}
+	println(token)
 
 	//Make sure the token is valid
 
@@ -72,5 +60,5 @@ func ValidAccessToken(authzHeader string) (map[string]interface{},error) {
 
 	//Make sure it includes a sub claim
 
-	return nil,nil
+	return nil, nil
 }
--- rollauthz/authz_test.go
+++ rollauthz/authz_test.go
@@ -1,31 +1,51 @@
 package rollauthz
+
 import (
-	"testing"
 	"github.com/stretchr/testify/assert"
-	"os"
+	"github.com/xtraclabs/rollsecrets/secrets"
+	"github.com/xtraclabs/rollsecrets/secrets/mocks"
+	rolltoken "github.com/xtraclabs/rollsecrets/token"
+	"testing"
 )
 
-func TestMisconfiguration(t *testing.T) {
-	os.Unsetenv("VAULT_ADDR")
-	os.Unsetenv("VAULT_TOKEN")
+const (
+	aToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBsaWNhdGlvbiI6ImRldiBwb3J0YWwiLCJhdWQiOiIzNGU0M2YwYS04N2RlLTQ3OTMtNTJjOS1iZGQ1MTgyNGYwNWMiLCJleHAiOjE0NTY5MjQ3OTEsImlhdCI6MTQ1NjgzODM5MSwianRpIjoiNDJjYmNiZDItZGQ5OS00NWVlLTcwMzYtNTM5ZjU5YjM1Y2FmIiwic2NvcGUiOiIiLCJzdWIiOiJ1c2VyIn0.no-7QKPf0XrrJiq44dWDHoirpxOR2N0mzvyrihxllv8TUix-vQdjao0fvHjzUA2X9rZWOcXmZC6zzJDlaF0kVO-mwSAa74btZI4oxsp4zRX_mtwwo5THsktAKcedzWezB-SrQqV-8NrNEjLbdl27rAydvAfc14bp9EV67fzyQws"
+)
 
-	_, err := ValidAccessToken("sure")
+func TestNonBearerTokenHeaderValuesRejected(t *testing.T) {
+	raz := RollAuthZ{}
+	claims, err := raz.ValidateAccessToken("not a bearer token beader")
+	assert.Nil(t, claims)
 	if assert.NotNil(t, err) {
-		_, ok := err.(ErrConfig)
-		assert.True(t, ok)
+		_, ok := err.(ErrNonBearerHeader)
+		assert.True(t, ok, "expected a ErrNonBearerHeader error")
 	}
 }
 
-func TestNonBearerTokenHeaderValuesRejected(t *testing.T) {
-	os.Setenv("VAULT_ADDR", "config")
-	os.Setenv("VAULT_TOKEN", "config")
-	initFromEnv()
-
-
-	claims, err := ValidAccessToken("not a bearer token beader")
-	assert.Nil(t,claims)
+func TestUnparsableToken(t *testing.T) {
+	raz := RollAuthZ{}
+	claims, err := raz.ValidateAccessToken("Bearer whoops")
+	assert.Nil(t, claims)
 	if assert.NotNil(t, err) {
-		_,ok := err.(ErrNonBearerHeader)
-		assert.True(t, ok, "expected a ErrNonBearerHeader error")
+		_, ok := err.(ErrParse)
+		assert.True(t, ok, "expected a ErrParse error")
 	}
+}
+
+func TestParseGoodToken(t *testing.T) {
+	privateKey, publicKey, err := secrets.GenerateKeyPair()
+	assert.Nil(t, err)
+
+	secretsMock := new(mocks.SecretsRepo)
+	secretsMock.On("RetrievePrivateKeyForApp", "1111-2222-3333333-4444444").Return(privateKey, nil)
+	secretsMock.On("RetrievePublicKeyForApp", "1111-2222-3333333-4444444").Return(publicKey, nil)
+
+	token, err := rolltoken.GenerateToken("a-subject", "", "1111-2222-3333333-4444444", "app name", privateKey)
+	assert.Nil(t, err)
+
+	raz := RollAuthZ{
+		secretsMock,
+	}
+	_, err = raz.ValidateAccessToken("Bearer " + token)
+	assert.Nil(t, err)
 }
